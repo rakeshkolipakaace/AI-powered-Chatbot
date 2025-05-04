f7ae19d001de6a1e6dc7ad1fa8302bfd5a76567b
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-04 16:12:18 +0530

    new updates

--- config/config.py
+++ config/config.py
@@ -1,3 +1,13 @@
-GEMINI_API_KEY = "AIzaSyDllhh8PF8P27IFP6B4EGCjJ87wlhu8rUg"  # Replace with your actual Gemini API key
-MONGODB_URI = "mongodb://localhost:27017/"
+# GEMINI_API_KEY = "AIzaSyDllhh8PF8P27IFP6B4EGCjJ87wlhu8rUg"  # Replace with your actual Gemini API key
+# MONGODB_URI = "mongodb://localhost:27017/"
+# DB_NAME = "edu_chatbot"
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MONGODB_URI = os.getenv("MONGODB_URI")
 DB_NAME = "edu_chatbot"
